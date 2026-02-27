d6f4d7529b8d04288334a288be98d013a65931ed
01max <m.louguet@gmail.com>
2026-02-27 22:46:02 +0100

    Populate default config.

--- lib/tocdoc/default.rb
+++ lib/tocdoc/default.rb
@@ -1,8 +1,87 @@
 # frozen_string_literal: true
 
+require "faraday"
+require "faraday/retry"
+
 module Tocdoc
-  # Tocdoc::Default
+  # Default configuration values and helpers for Tocdoc.
   module Default
-    # Default configuration values will be defined in later phases.
+    API_ENDPOINT = "https://www.doctolib.fr".freeze
+    USER_AGENT   = "Tocdoc Ruby Gem #{Tocdoc::VERSION}".freeze
+    MEDIA_TYPE   = "application/json".freeze
+    PER_PAGE     = 5
+    MAX_RETRY    = 3
+
+    class << self
+      # Returns a hash of default configuration options.
+      def options
+        {
+          api_endpoint: api_endpoint,
+          user_agent: user_agent,
+          default_media_type: default_media_type,
+          per_page: per_page,
+          middleware: middleware,
+          connection_options: connection_options
+        }
+      end
+
+      def api_endpoint
+        ENV.fetch("TOCDOC_API_ENDPOINT", API_ENDPOINT)
+      end
+
+      def user_agent
+        ENV.fetch("TOCDOC_USER_AGENT", USER_AGENT)
+      end
+
+      def default_media_type
+        ENV.fetch("TOCDOC_MEDIA_TYPE", MEDIA_TYPE)
+      end
+
+      def per_page
+        Integer(ENV.fetch("TOCDOC_PER_PAGE", PER_PAGE), 10)
+      rescue ArgumentError
+        PER_PAGE
+      end
+
+      # Default Faraday middleware stack: retry + error handling + adapter.
+      def middleware
+        @middleware ||= build_middleware
+      end
+
+      # Default Faraday connection options.
+      def connection_options
+        @connection_options ||= {}
+      end
+
+      private
+
+      def build_middleware
+        Faraday::RackBuilder.new do |builder|
+          builder.request :retry, retry_options
+          builder.use Tocdoc::Middleware::RaiseError
+          builder.adapter Faraday.default_adapter
+        end
+      end
+
+      def retry_options
+        {
+          max: Integer(ENV.fetch("TOCDOC_RETRY_MAX", MAX_RETRY), 10),
+          interval: 0.5,
+          interval_randomness: 0.5,
+          backoff_factor: 2,
+          retry_statuses: [429, 500, 502, 503, 504],
+          methods: %i[get head options]
+        }
+      rescue ArgumentError
+        {
+          max: 3,
+          interval: 0.5,
+          interval_randomness: 0.5,
+          backoff_factor: 2,
+          retry_statuses: [429, 500, 502, 503, 504],
+          methods: %i[get head options]
+        }
+      end
+    end
   end
 end
