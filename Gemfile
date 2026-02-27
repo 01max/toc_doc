3fee5a01cc1525539bca62975868566b39df644d
01max <m.louguet@gmail.com>
2026-02-27 22:39:14 +0100

    Config spec VCR + Webmock.

--- Gemfile
+++ Gemfile
@@ -11,3 +11,8 @@ gem "rake", "~> 13.0"
 gem "rspec", "~> 3.0"
 
 gem "rubocop", "~> 1.21"
+
+group :test do
+	gem "webmock", "~> 3.0"
+	gem "vcr", "~> 6.0"
+end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,22 @@
 # frozen_string_literal: true
 
+require "bundler/setup"
 require "tocdoc"
+require "webmock/rspec"
+require "vcr"
+
+SPEC_ROOT = File.expand_path("..", __dir__)
+
+VCR.configure do |config|
+  config.cassette_library_dir = File.join(SPEC_ROOT, "fixtures", "vcr_cassettes")
+  config.hook_into :webmock
+  config.configure_rspec_metadata!
+
+  # Allow localhost connections (for debugging) and optionally other hosts.
+  config.ignore_hosts "127.0.0.1", "localhost"
+end
+
+WebMock.disable_net_connect!(allow_localhost: true)
 
 RSpec.configure do |config|
   # Enable flags like --only-failures and --next-failure
@@ -11,5 +27,20 @@ RSpec.configure do |config|
 
   config.expect_with :rspec do |c|
     c.syntax = :expect
+  end
+
+  # Automatically tag examples with VCR metadata via :vcr tag
+  config.around(:each, :vcr) do |example|
+    name = example.full_description.downcase.gsub(/[^a-z0-9]+/, "_").gsub(/(^_|_$)/, "")
+    VCR.use_cassette(name, record: :once, &example)
+  end
+
+  # Helper for loading static fixtures
+  def fixture_path
+    File.join(SPEC_ROOT, "fixtures")
+  end
+
+  def fixture(file)
+    File.read(File.join(fixture_path, file))
   end
 end
