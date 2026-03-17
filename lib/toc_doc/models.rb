5c76f5aea2adbb631a7b437b057a52ad40f44c38
01max <m.louguet@gmail.com>
2026-03-17 21:04:05 +0100

    Update models requires.

--- lib/toc_doc/models.rb
+++ lib/toc_doc/models.rb
@@ -6,5 +6,6 @@ require 'toc_doc/models/search'
 require 'toc_doc/models/availability'
 require 'toc_doc/models/availability/collection'
 
+require 'toc_doc/models/place'
 require 'toc_doc/models/profile'
 require 'toc_doc/models/speciality'
