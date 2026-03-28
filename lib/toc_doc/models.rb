4c81f01e314a6886d02ecc82c70546f840c44db2
01max <m.louguet@gmail.com>
2026-03-28 21:24:59 +0100

    Expose booking_info top-level method and wire model requires

--- lib/toc_doc/models.rb
+++ lib/toc_doc/models.rb
@@ -3,6 +3,9 @@
 require 'toc_doc/models/resource'
 require 'toc_doc/models/speciality'
 require 'toc_doc/models/place'
+require 'toc_doc/models/visit_motive'
+require 'toc_doc/models/agenda'
+require 'toc_doc/models/booking_info'
 require 'toc_doc/models/search'
 
 require 'toc_doc/models/availability'
